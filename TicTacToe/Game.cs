793df1399c48f93a87ec6caa7de7376eec7b8821
Nate Grabowski <nategrabowski@outlook.com>
2022-11-07 18:56:11 -0700

    Tic Tac Toe -semi functional

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -8,11 +8,11 @@ namespace TicTacToe
 {
     public class Game
     {
-        public int Turn = 0; //Player 1 is 0 && Player 2 is 1
+        public int Turn = 0; //Player 1 && Player 2 1
+        //private string e1, e2, e3, e4, e5, e6, e7, e8, e9;
         private string e1 = " ", e2 = " ", e3 = " ", e4 = " ", e5 = " ", e6 = " ", e7 = " ", e8 = " ", e9 = " ";
         public Input input { get; set; }
         public Section section { get; set; }
-
         public enum Input { X, O }
         public enum Section { BottomLeft, BottomMiddle, BottomRight, MiddleLeft, MiddleMiddle, MiddleRight, TopLeft, TopMiddle, TopRight }
 
@@ -20,136 +20,128 @@ namespace TicTacToe
         {
             while (true) //result of tie & winner should end game
             {
+                Console.Title = "Tic-Tac-Toe Console App";
+                DisplayBoard();
                 Console.WriteLine("Where would you like to place your input? Use the number pad to guide your choice.");
+                Console.Write($"Player {Turn}: ");
                 int input = Convert.ToInt16(Console.ReadLine());
                 switch (input)
                 {
                     case 1:
-                        Board(Turn, Section.BottomLeft);
+                        PlaceMark(Turn, Section.BottomLeft);
                         break;
                     case 2:
-                        Board(Turn, Section.BottomMiddle);
+                        PlaceMark(Turn, Section.BottomMiddle);
                         break;
                     case 3:
-                        Board(Turn, Section.BottomRight);
+                        PlaceMark(Turn, Section.BottomRight);
                         break;
                     case 4:
-                        Board(Turn, Section.MiddleLeft);
+                        PlaceMark(Turn, Section.MiddleLeft);
                         break;
                     case 5:
-                        Board(Turn, Section.MiddleMiddle);
+                        PlaceMark(Turn, Section.MiddleMiddle);
                         break;
                     case 6:
-                        Board(Turn, Section.MiddleRight);
+                        PlaceMark(Turn, Section.MiddleRight);
                         break;
                     case 7:
-                        Board(Turn, Section.TopLeft);
+                        PlaceMark(Turn, Section.TopLeft);
                         break;
                     case 8:
-                        Board(Turn, Section.TopMiddle);
+                        PlaceMark(Turn, Section.TopMiddle);
                         break;
                     case 9:
-                        Board(Turn, Section.TopRight);
+                        PlaceMark(Turn, Section.TopRight);
                         break;
                 }
-
             }
         }
-        public void Board(int turn, Section section)
+        public void PlaceMark(int turn, Section section)
         {
-            bool isSectionTaken = CheckIfSpotTaken();
-            if (!isSectionTaken)
+            switch (turn, section)
             {
-                switch (turn, section)
-                {
-                    case (0, Section.BottomLeft):
-                        e1 = Enum.GetName(Input.X);
-                        break;
-                    case (0, Section.BottomMiddle):
-                        e2 = Enum.GetName(Input.X);
-                        break;
-                    case (0, Section.BottomRight):
-                        e3 = Enum.GetName(Input.X);
-                        break;
-                    case (0, Section.MiddleLeft):
-                        e4 = Enum.GetName(Input.X);
-                        break;
-                    case (0, Section.MiddleMiddle):
-                        e5 = Enum.GetName(Input.X);
-                        break;
-                    case (0, Section.MiddleRight):
-                        e6 = Enum.GetName(Input.X);
-                        break;
-                    case (0, Section.TopLeft):
-                        e7 = Enum.GetName(Input.X);
-                        break;
-                    case (0, Section.TopMiddle):
-                        e8 = Enum.GetName(Input.X);
-                        break;
-                    case (0, Section.TopRight):
-                        e9 = Enum.GetName(Input.X);
-                        break;
-                    case (1, Section.BottomLeft):
-                        e1 = Enum.GetName(Input.O);
-                        break;
-                    case (1, Section.BottomMiddle):
-                        e2 = Enum.GetName(Input.O);
-                        break;
-                    case (1, Section.BottomRight):
-                        e3 = Enum.GetName(Input.O);
-                        break;
-                    case (1, Section.MiddleLeft):
-                        e4 = Enum.GetName(Input.O);
-                        break;
-                    case (1, Section.MiddleMiddle):
-                        e5 = Enum.GetName(Input.O);
-                        break;
-                    case (1, Section.MiddleRight):
-                        e6 = Enum.GetName(Input.O);
-                        break;
-                    case (1, Section.TopLeft):
-                        e7 = Enum.GetName(Input.O);
-                        break;
-                    case (1, Section.TopMiddle):
-                        e8 = Enum.GetName(Input.O);
-                        break;
-                    case (1, Section.TopRight):
-                        e9 = Enum.GetName(Input.O);
-                        break;
-                }
+                case (0, Section.BottomLeft):
+                    if (e1 == " ") { e1 = Enum.GetName(Input.X); }
+                    break;
+                case (0, Section.BottomMiddle):
+                    if (e2 == " ") { e2 = Enum.GetName(Input.X); }
+                    break;
+                case (0, Section.BottomRight):
+                    if (e3 == " ") { e3 = Enum.GetName(Input.X); }
+                    break;
+                case (0, Section.MiddleLeft):
+                    if (e4 == " ") { e4 = Enum.GetName(Input.X); }
+                    break;
+                case (0, Section.MiddleMiddle):
+                    if (e5 == " ") { e5 = Enum.GetName(Input.X); }
+                    break;
+                case (0, Section.MiddleRight):
+                    if (e6 == " ") { e6 = Enum.GetName(Input.X); }
+                    break;
+                case (0, Section.TopLeft):
+                    if (e7 == " ") { e7 = Enum.GetName(Input.X); }
+                    break;
+                case (0, Section.TopMiddle):
+                    if (e8 == " ") { e8 = Enum.GetName(Input.X); }
+                    break;
+                case (0, Section.TopRight):
+                    if (e9 == " ") { e9 = Enum.GetName(Input.X); }
+                    break;
+                case (1, Section.BottomLeft):
+                    if (e1 == " ") { e1 = Enum.GetName(Input.O); }
+                    break;
+                case (1, Section.BottomMiddle):
+                    if (e2 == " ") { e2 = Enum.GetName(Input.O); }
+                    break;
+                case (1, Section.BottomRight):
+                    if (e3 == " ") { e3 = Enum.GetName(Input.O); }
+                    break;
+                case (1, Section.MiddleLeft):
+                    if (e4 == " ") { e4 = Enum.GetName(Input.O); }
+                    break;
+                case (1, Section.MiddleMiddle):
+                    if (e5 == " ") { e5 = Enum.GetName(Input.O); }
+                    break;
+                case (1, Section.MiddleRight):
+                    if (e6 == " ") { e6 = Enum.GetName(Input.O); }
+                    break;
+                case (1, Section.TopLeft):
+                    if (e7 == " ") { e7 = Enum.GetName(Input.O); }
+                    break;
+                case (1, Section.TopMiddle):
+                    if (e8 == " ") { e8 = Enum.GetName(Input.O); }
+                    break;
+                case (1, Section.TopRight):
+                    if (e9 == " ") { e9 = Enum.GetName(Input.O); }
+                    break;
             }
-            else
-            {
+        }
 
-                Console.WriteLine("Sorry that spot is already taken!");
-                //Board(turn, section); TODO: replay turn
-            }
-            TurnChanger();
+        //public bool CheckIfSpotTaken()
+        //{
+        //    string[] eValues = { e1, e2, e3, e4, e5, e6, e7, e8, e9 };
+
+
+
+
+        //    //bool isSectionTaken = false;
+        //    //for (int i = 0; i < eValues.Length; i++)
+        //    //{
+        //    //    if (eValues[i] == "X" || eValues[i] == "O") { isSectionTaken = true; }
+        //    //}
+
+        //}
+        public void DisplayBoard()
+        {
+            Console.Clear();
             Console.WriteLine($" {e7} | {e8} |{e9}  "); //top row
             Console.WriteLine($"---+---+---"); //top line
             Console.WriteLine($" {e4} | {e5} |{e6}  "); //middle row
             Console.WriteLine($"---+---+---"); //top line //middle line
             Console.WriteLine($" {e1} | {e2} |{e3}   "); //bottom row
-
         }
-        public bool CheckIfSpotTaken()
-        {
-            string[] eValues = { e1, e2, e3, e4, e5, e6, e7, e8, e9 };
-            bool isSectionTaken = false;
-            for (int i = 0; i < eValues.Length; i++)
-            {
-                if (eValues[i] == "X" || eValues[i] == "O") { isSectionTaken = true; }
-            }
 
-            if (true)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
         public int TurnChanger()
         {
             return Turn == 0 ? Turn += 1 : Turn -= 1;
