936fac8b8b6a6044a0242fd980a7680b79884e21
Nate Grabowski <nategrabowski@outlook.com>
2022-11-10 00:14:14 -0700

    ++Old Robot completed

--- The_Old_Robot/Program.cs
+++ The_Old_Robot/Program.cs
@@ -1,25 +1,26 @@
-﻿Robot r1 = new Robot();
-r1 = new OnCommand();
+﻿int i = 0;
+Robot robo = new Robot();
+Console.WriteLine("You come across this old robot in the sand...what would you like to do?");
+Console.WriteLine("1 - Turn On\n2 - Turn Off\n3 - Move North\n4 - Move South\n5 - Move West\n6 - Move East");
 
 
-
-
-
-//while (true)
-//{
-
-//    Console.WriteLine("You come across this old robot in the sand...what would you like to do?");
-//    Console.WriteLine("1 - Turn On\n2 - Turn Off\n3 - Move North\n4 - Move South\n5 - Move West\n6 - Move East");
-//    int input = Convert.ToInt16(Console.ReadLine());
-//    Robot robo = input switch
-//    {
-//        1 => new Robot().Commands
-//    };
-
-//    Console.WriteLine($"Location of Robot [{},{}]");
-//    //Console.WriteLine("Beep Boop Beep Boop");
-//}
-
+while (i != 3)
+{
+    int input = Convert.ToInt16(Console.ReadLine());
+    RobotCommand RC = input switch
+    {
+        1 => new OnCommand(),
+        2 => new OffCommand(),
+        3 => new NorthCommand(),
+        4 => new SouthCommand(),
+        5 => new WestCommand(),
+        6 => new EastCommand(),
+    };
+    RC.Run(robo);
+    i++;
+}
+Console.WriteLine("Beep Boop Beep Boop");
+robo.Run();
 
 
 public class Robot
